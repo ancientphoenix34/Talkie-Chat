adf349d539c08100572e1f42a571bc9191321e61
ancientphoenix34 <farsukoothradan@gmail.com>
2024-12-13 20:42:47 +0530

    desigining has begun,Context part done

--- frontend/src/Pages/Chats.jsx
+++ frontend/src/Pages/Chats.jsx
@@ -1,11 +1,31 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
+import { ChatState } from '../Context/ChatProvider'
+import { Box } from '@chakra-ui/react';
+import SideDrawer from '../Components/miscellaneous/SideDrawer';
+import MyChats from '../Components/MyChats';
+import ChatBox from '../Components/ChatBox';
 
 const Chats = () => {
+ const {user}=ChatState();
 
   
-  return (
-    <div>Chats</div>
+  return ( <div style={{width:"100%"}}>
+    {/* conditional rendering */}
+    {/* user is a variable that is being evaluated as a boolean value (i.e., true or false).
+    The && operator is a logical AND operator. It will only render the component <MyChats/> if the condition user is true. */}
+
+    {user && <SideDrawer/>}
+    <Box 
+    d='flex'
+    justifyContent="space-between"
+    w="100%"
+    h='91.5vh'
+    >
+      {user && <MyChats/>}
+      {user && <ChatBox/>}
+    </Box>
+      </div>
   )
 }
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,15 +4,19 @@ import './index.css';
 import App from './App';
 import { ChakraProvider, defaultSystem } from '@chakra-ui/react';
 import { BrowserRouter } from 'react-router-dom';
+import ChatProvider from './Context/ChatProvider';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
+  
   <React.StrictMode>
     <BrowserRouter>
+    <ChatProvider>
     <ChakraProvider value={defaultSystem}>
     <App />
     </ChakraProvider>
+    </ChatProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
