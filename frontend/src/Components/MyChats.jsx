1806c57c75d7f5598faa8f6cd07ede0fb27ea4de
ancientphoenix34 <farsukoothradan@gmail.com>
2025-01-04 19:57:59 +0530

    chat part done

--- frontend/src/Components/MyChats.jsx
+++ frontend/src/Components/MyChats.jsx
@@ -2,9 +2,9 @@ import React, { useEffect, useState } from 'react'
 import { ChatState } from '../Context/ChatProvider';
 import {  toaster, Toaster } from "../Components/ui/toaster";
 import axios from 'axios';
-import { Box, Button } from '@chakra-ui/react';
-import { LuActivity } from 'react-icons/lu';
-
+import { Box, Button, Stack, Text } from '@chakra-ui/react';
+import ChatLoading from './ChatLoading';
+import { getSender } from '../config/ChatLogics';
 const MyChats = () => {
   
   const [loggedUser,setLoggedUser]=useState();
@@ -37,38 +37,75 @@ toaster.create({
   },[]);
   
   return (
-    <div>
+    <>
       <Toaster/>
      <Box
      d={{base:selectedChat? "none":"flex",md:"flex"}}
-     flexDir={"column"}
-     p={3}
-     bg={"white"}
+     flexDir="column"
+     p="3"
+     bg="white"
      w={{base:"100%",md:"31%"}}
-     borderRadius={"lg"}
-     borderWidth={"1px"}
+     borderRadius="lg"
+     borderWidth="1px"
      >
     <Box
     pb={3}
     px={3}
     fontSize={{base:"28px",md:"30px"}}
-    fontFamily={"Work sans"}
-    d={"flex"}
-    w={"100%"}
-    justifyContent={"space-between"}
-    alignItems={"center"}
+    fontFamily="Work sans"
+    d="flex"
+    w="100%"
+    justifyContent="space-between"
+    alignItems="center"
     >
 My Chats
 <Button
 d="flex"
 fontSize={{base:"17px",md:"10px",lg:"17px"}}
-rightIcon={<LuActivity/>}
->New Group Chat</Button>
+>New Group Chat +</Button>
+    </Box>
+
+    <Box
+    d="flex"
+    flexDir="column"
+    p={3}
+    bg="#F8F8F8"
+    w="100%"
+    h="100%"
+    borderRadius="lg"
+    overflowY="hidden"
+    >
+   {chats?(
+<Stack overflowY="scroll">
+  {
+    chats.map((chat)=>(
+      <Box
+      onClick={()=>setSelectedChat(chat)}
+      cursor="pointer"
+      bg={selectedChat===chat?"#38B2AC":"#E8E8E8"}
+      color={selectedChat===chat?"white":"#000000"}
+      px={3}
+      py={2}
+      borderRadius="lg"
+      key={chat._id}
+      >
+        <Text>
+          {!chat.isGroupChat?(
+            getSender(loggedUser,chat.users)
+          ):chat.chatName}
+        </Text>
+      </Box>
+    ))
+  }
+  </Stack>
+   ):(
+    <ChatLoading/>
+   )}
     </Box>
      </Box>
       
             
-    </div>
+    </>
   )
 }
 
